f4a34ef3ef0749ac3db3e730ba91da8a608d6ca5
stan-dot <stanislawmalinowski09@gmail.com>
2023-10-19 11:15:02 +0100

    fixed the storybook error

--- my-app/.storybook/main.ts
+++ my-app/.storybook/main.ts
@@ -2,12 +2,14 @@
 const config = {
   stories: [
     "../components/**/*.mdx",
-    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx|vue)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+       "storybook-vue-addon"
+
   ],
   framework: {
     name: "@storybook/vue3-vite",
